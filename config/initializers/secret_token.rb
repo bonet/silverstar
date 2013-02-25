082d9af1f02349d14acdfff5b6ec0d9c97033075
unknown <User@.(none)>
2013-02-25 22:24:58 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Silverstar::Application.config.secret_token = '92dd8ab30b945b137475719dad4af96f1e9e59853721537a1716d376bcd31bbfc5682bec46b7d17ce3a55f3a9027c3669000ef589e6f224faae05939676efcf9'
